54b080469cd9607626514521e7a2df10d638ce4f
Paul C. Jennings <jennings.p.c@gmail.com>
2017-10-29 14:20:23 -0700

    add a really basic test case

---
+++ tests/run_search.py
@@ -0,0 +1,25 @@
+import unittest
+import random
+
+from genetic import GeneticAlgorithm
+
+
+class TestGeneticAlgorithm(unittest.TestCase):
+    def ff(self, x):
+        """Some random fitness is returned."""
+        return random.random()
+
+    def test_run_ga(self):
+        """Simple test case to make sure it doesn't crash."""
+        ga = GeneticAlgorithm(pop_size=10,
+                              fit_func=self.ff,
+                              d_param=[1, 100, 3],
+                              pop=None)
+        ga.search(500)
+
+        self.assertTrue(len(ga.pop) == 10)
+        self.assertTrue(len(ga.fitness) == 10)
+
+
+if __name__ == '__main__':
+    unittest.main()
